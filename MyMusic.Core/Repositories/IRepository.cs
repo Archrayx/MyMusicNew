498ee535347a01c203252a337d0cbd2fae55e900
Xavier Rivera <riverax34@gmail.com>
2022-01-11 12:00:04 -0500

    Add project files.

---
+++ MyMusic.Core/Repositories/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        ValueTask<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        //find function takes in a delegate expression named predicate, which accepts parameter TEntity and returns boolean.
+        //Func Denotes delegate, Expression is linq way of turning it into a expression tree or a search function that linq understands
+        //basically the below returns a list of found items from list in entity. it takes in a search predicate, which is a function 
+        //it will use as a definition for search through the entity.
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate); 
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);    
+    }
+}
---
+++ MyMusic.Core/Repositories/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Core.Repositories
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        IMusicRepository Musics { get; }
+        IArtistRepository Artists { get; }
+        Task<int> CommitAsync();
+    }
+}
